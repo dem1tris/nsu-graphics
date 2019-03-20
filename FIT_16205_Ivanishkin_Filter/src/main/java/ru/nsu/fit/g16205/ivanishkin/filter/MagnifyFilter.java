bf325da24bcf9d68d8e821b8857911bafc1204b7
Dmitry Ivanishkin <d@ivanishkin.ru>
2019-03-20 13:38:35 +0700

    fix panel size, magnify filter partially impl

--- FIT_16205_Ivanishkin_Filter/src/main/java/ru/nsu/fit/g16205/ivanishkin/filter/MagnifyFilter.java
+++ FIT_16205_Ivanishkin_Filter/src/main/java/ru/nsu/fit/g16205/ivanishkin/filter/MagnifyFilter.java
@@ -2,9 +2,13 @@ package ru.nsu.fit.g16205.ivanishkin.filter;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
-import java.util.Random;
+import java.util.Arrays;
 
 public class MagnifyFilter implements Filter {
+    private static int[][] NEIGHBOURS = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+    private int width;
+    private int height;
+
     @Override
     public BufferedImage apply(BufferedImage target) {
         BufferedImage result = new BufferedImage(
@@ -14,26 +18,60 @@ public class MagnifyFilter implements Filter {
                 null);
 
         WritableRaster r = result.getRaster();
+        width = r.getWidth();
+        height = r.getHeight();
         int[] before = r.getPixels(0, 0, r.getWidth(), r.getHeight(), (int[]) null);
         int[] after = new int[before.length];
-        for (int i = 0; i < target.getHeight() * target.getWidth() * 3; i += 3) {
-            int x = i / 3 % r.getWidth();
-            int y = i / 3 / r.getWidth();
+        for (int i = 0; i < width * height * 3; i += 3) {
+            int x = toX(i);
+            int y = toY(i);
             if (x % 2 == 0 && y % 2 == 0) {
-                x = x / 2 + r.getWidth() / 4;
-                y = y / 2 + r.getHeight() / 4;
-                int ind = (y * r.getWidth() + x) * 3;
+                x = x / 2 + width / 4;
+                y = y / 2 + height / 4;
+                int ind = relInd(0, x, y);
                 after[i] = before[ind];
                 after[i + 1] = before[ind + 1];
                 after[i + 2] = before[ind + 2];
-            } else {
-
-
-                
             }
-
+        }
+        for (int i = 0; i < width * (height - 1) * 3; i += 3) {
+            int x = toX(i);
+            int y = toY(i);
+            if (x % 2 != 0 && y % 2 != 0) {
+                final int j = i;
+                after[i] = Arrays.stream(NEIGHBOURS).mapToInt(it -> after[relInd(j, it[0], it[1])]).sum() / 4;
+                after[i + 1] = Arrays.stream(NEIGHBOURS).mapToInt(it -> after[relInd(j, it[0], it[1]) + 1]).sum() / 4;
+                after[i + 2] = Arrays.stream(NEIGHBOURS).mapToInt(it -> after[relInd(j, it[0], it[1]) + 2]).sum() / 4;
+            }
+        }
+        for (int i = width * (height - 1) * 3; i < width * height * 3; i+=3) {
+            after[i] = (after[relInd(i, 0, -1)] * 2 + after[relInd(i, 0, -2)]) / 3;
+            after[i + 1] = (after[relInd(i, 0, -1) + 1] * 2 + after[relInd(i, 0, -2) + 1]) / 3;
+            after[i + 2] = (after[relInd(i, 0, -1) + 2] * 2 + after[relInd(i, 0, -2) + 2]) / 3;
         }
         r.setPixels(0, 0, r.getWidth(), r.getHeight(), after);
         return result;
+    }
+
+    private int toX(int i) {
+        return i / 3 % width;
+    }
+
+    private int toY(int i) {
+        return i / 3 / height;
+    }
+
+    /**
+     * Returns index of translated pixel
+     *
+     * @param i  - index of current pixel
+     * @param dx - translation by X
+     * @param dy - translation by Y
+     * @return relative index
+     */
+    private int relInd(int i, int dx, int dy) {
+        int x = toX(i);
+        int y = toY(i);
+        return ((y + dy) * width + x + dx) * 3;
     }
 }
--- FIT_16205_Ivanishkin_Filter/src/main/java/ru/nsu/fit/g16205/ivanishkin/view/ImageView.java
+++ FIT_16205_Ivanishkin_Filter/src/main/java/ru/nsu/fit/g16205/ivanishkin/view/ImageView.java
@@ -3,12 +3,17 @@ package ru.nsu.fit.g16205.ivanishkin.view;
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 import static java.lang.Math.round;
 
 public class ImageView extends JPanel {
+    protected static final int PANEL_SIZE = 354;
     protected static final int SIZE = 350;
+    protected static final int X = 2;
+    protected static final int Y = 2;
 
     protected BufferedImage image;
     protected Dimension paintSize = new Dimension(0, 0);
@@ -16,16 +21,22 @@ public class ImageView extends JPanel {
 
     ImageView() {
         setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
-        Dimension preferredSize = new Dimension(SIZE, SIZE);
+        Dimension preferredSize = new Dimension(PANEL_SIZE, PANEL_SIZE);
         setPreferredSize(preferredSize);
         setMinimumSize(preferredSize);
         setMaximumSize(preferredSize);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.err.println(e.getX() + " " + e.getY());
+            }
+        });
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, paintSize.width, paintSize.height, this);
+        g.drawImage(image, X, Y, paintSize.width, paintSize.height, this);
     }
 
     public BufferedImage getImage() {
